982f8e04859bc7d3d76f987895cc43cef4a56f5f
SindreSB <sindre.brun@outlook.com>
2016-05-01 16:15:19 +0200

    Initial windows commit

---
+++ windows/Teknobyen/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Template10.Services.NavigationService;
+using Windows.UI.Xaml.Navigation;
+using Teknobyen.Views;
+
+namespace Teknobyen.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+        public void GotoClothesWashMenu()
+        {
+            this.NavigationService.Navigate(typeof(ClothesWashMenuView));
+        }
+    }
+}
+
