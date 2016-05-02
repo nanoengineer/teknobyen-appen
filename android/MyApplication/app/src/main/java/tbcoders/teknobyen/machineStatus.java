da89d35ed3d6aae293ab2750d2183791d933402d
Sølve Robert Bø Hunvik <srb.hunvik@gmail.com>
2016-05-02 20:15:47 +0200

    Nettsider for vasking fungerar i appen + brukerdataoppbevaring

--- android/MyApplication/app/src/main/java/tbcoders/teknobyen/machineStatus.java
+++ android/MyApplication/app/src/main/java/tbcoders/teknobyen/machineStatus.java
@@ -1,5 +1,6 @@
 package tbcoders.teknobyen;
 
+import android.content.SharedPreferences;
 import android.net.http.SslError;
 import android.os.Build;
 import android.support.v7.app.AlertDialog;
@@ -11,7 +12,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class machineStatus extends AppCompatActivity {
+public class MachineStatus extends AppCompatActivity {
     //This class is for opening the web browser inside the app.
     //But it doesn't work yet because I get an access denied error when trying to open the webpage.
 
@@ -19,14 +20,18 @@ public class machineStatus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_machine_status);
+        SharedPreferences sharedPref = getSharedPreferences("mypref", 0);
+        String name = sharedPref.getString("username", "");
+        String password = sharedPref.getString("password", "");
+
+
         WebView webView = (WebView)findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new MyWebViewClient());
         webView.getSettings().setSupportMultipleWindows(true);
+        webView.setHttpAuthUsernamePassword("http://129.241.152.11/", "", name, password);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         webView.getSettings().setDomStorageEnabled(true);
-
         webView.setVerticalScrollBarEnabled(false);
         webView.loadUrl("http://129.241.152.11/LaundryState?lg=2&ly=9131");
     }
--- android/MyApplication/app/src/main/java/tbcoders/teknobyen/washingMachine.java
+++ android/MyApplication/app/src/main/java/tbcoders/teknobyen/washingMachine.java
@@ -1,30 +1,57 @@
 package tbcoders.teknobyen;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
-public class washingMachine extends AppCompatActivity {
+public class WashingMachine extends AppCompatActivity {
     private static ImageButton imageButton;
+    private static Button loginbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_washing_machine);
         OnClickStatusListener();
         OnClickRefillListener();
+        OnClickLoginListener();
     }
+
+    private void OnClickLoginListener() {
+        loginbtn = (Button)findViewById(R.id.alertLogin);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WashingMachine.this, LoginActivityWash.class);
+                startActivity(intent);
+            }
+        });
+    }
+
     public void OnClickStatusListener(){
+
         imageButton = (ImageButton) findViewById(R.id.BTN_washingS);
         imageButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        //Intent intent = new Intent(washingMachine.this, machineStatus.class);
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://129.241.152.11/LaundryState?lg=2&ly=9131"));
-                        startActivity(intent);
+                        SharedPreferences myprefs = getSharedPreferences("mypref", 0);
+                        String userS = myprefs.getString("username", "");
+                        String userP = myprefs.getString("password", "");
+                        if(userS.length()>0 && userP.length()>0){
+                            Intent intent = new Intent(WashingMachine.this, MachineStatus.class);
+                            startActivity(intent);
+                        }else{
+                            Intent intent = new Intent(WashingMachine.this, LoginActivityWash.class);
+                            startActivity(intent);
+                        }
+                        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://129.241.152.11/LaundryState?lg=2&ly=9131"));
+
                     }
                 }
         );
@@ -35,9 +62,17 @@ public class washingMachine extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        //Intent intent = new Intent(washingMachine.this, machineStatus.class)
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://129.241.152.11/SaldoForm?lg=2&ly=9131"));
-                        startActivity(intent);
+                        SharedPreferences myprefs = getSharedPreferences("mypref", 0);
+                        String userS = myprefs.getString("username", "");
+                        String userP = myprefs.getString("password", "");
+                        if(userS.length()>0 && userP.length()>0){
+                            Intent intent = new Intent(WashingMachine.this, MachineRefill.class);
+                            startActivity(intent);
+                        }else{
+                            Intent intent = new Intent(WashingMachine.this, LoginActivityWash.class);
+                            startActivity(intent);
+                        }
+                        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://129.241.152.11/SaldoForm?lg=2&ly=9131"));
                     }
                 }
         );
