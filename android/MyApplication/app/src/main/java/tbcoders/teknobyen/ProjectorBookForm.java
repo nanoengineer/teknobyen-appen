6bb55ce9c5ae3373008c67e93f4883024e1f047a
Sølve Robert Bø Hunvik <srb.hunvik@gmail.com>
2016-05-14 20:39:46 +0200

    Fiksa format på ProjectorBookForm. Skal lage reset knapp seinare osv.

--- android/MyApplication/app/src/main/java/tbcoders/teknobyen/ProjectorBookForm.java
+++ android/MyApplication/app/src/main/java/tbcoders/teknobyen/ProjectorBookForm.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import com.firebase.client.Firebase;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,23 +29,24 @@ public class ProjectorBookForm extends AppCompatActivity {
     private final String[] minValues = {"00", "15", "30", "45"};
     private final String[] hourValues = {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"};
     private final String[] weekDays = {"Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"};
+    private final String[] hourDurations = {"0 timer", "1 time", "2 timer", "3 timer", "4 timer"};
+    private final String[] minuteDurations = {"0 min", "15 min", "30 min", "45 min"};
     private int startDayValue = 0;
     private int startHourValue = 0;
     private int startMinValue = 0;
-    private int endDayValue = 5;
-    private int endHourValue = 23;
-    private int endMinValue = 45;
+    private String startDayName = "";
+    private int durationHour = 0;
+    private int durationMin = 0;
+    private boolean startMsgSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projector_book_form);
         OnClickStartListener();
-        OnClickEndListener();
         OnClickReserveListener();
         setupNumberPickers();
     }
-
     public void setupNumberPickers() {
         pickDate = (NumberPicker) findViewById(R.id.datePicker);
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Oslo"));
@@ -70,13 +72,13 @@ public class ProjectorBookForm extends AppCompatActivity {
         pickHour.setDisplayedValues(hourValues);
         pickHour.setMaxValue(23);
         pickHour.setMinValue(0);
-        pickHour.setWrapSelectorWheel(false);
+        pickHour.setWrapSelectorWheel(true);
 
         pickMin = (NumberPicker) findViewById(R.id.startMinutePicker);
         pickMin.setMinValue(0);
         pickMin.setMaxValue(3);
         pickMin.setDisplayedValues(minValues);
-        pickMin.setWrapSelectorWheel(false);
+        pickMin.setWrapSelectorWheel(true);
     }
 
     public void OnClickStartListener() {
@@ -92,35 +94,22 @@ public class ProjectorBookForm extends AppCompatActivity {
                 int smValue = pickMin.getValue();
                 int trueMinValue = Integer.parseInt(minValues[smValue]);
                 //Kontrollere om starttid er før slutttid før skriving og større enn current time
-                if (controlCurrentTime(sdValue, shValue, trueMinValue)) {
-                    if (sdValue < endDayValue) {
-                        writeStartTimeMessage();
-                    } else if (sdValue > endDayValue) {
-                        endDayValue = 5;
-                        endHourValue = 23;
-                        endMinValue = 45;
-                        writeStartTimeMessage();
-                        TextView endText = (TextView) findViewById(R.id.endtimeString);
-                        endText.setText("");
+                if(startMsgSet == false){
+                    if (controlCurrentTime(sdValue, shValue, trueMinValue)) {
+                        writeStartTimeMessage(sdValue, shValue, smValue);
                     } else {
-                        if (shValue < endHourValue || shValue == endHourValue && trueMinValue < endMinValue) {
-                            writeStartTimeMessage();
-                        } else {
-                            endDayValue = 5;
-                            endHourValue = 23;
-                            endMinValue = 45;
-                            writeStartTimeMessage();
-                            TextView endText = (TextView) findViewById(R.id.endtimeString);
-                            endText.setText("");
-                        }
+                        Toast.makeText(ProjectorBookForm.this, "Verdi for starttid må være større enn nåværende klokkeslett", Toast.LENGTH_SHORT).show();
                     }
-                } else {
-                    alertMsg('w');
+                }else{
+                    if(shValue > 0 || smValue > 0){
+                        writeEndTimeMessage(shValue, smValue);
+                    }else{
+                        Toast.makeText(ProjectorBookForm.this, "Varighet må være større enn null", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
     }
-
     public boolean controlCurrentTime(int day, int hour, int minute) {
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Oslo"));
         int currentHour = cal.get(Calendar.HOUR_OF_DAY);
@@ -140,78 +129,60 @@ public class ProjectorBookForm extends AppCompatActivity {
         return false;
     }
 
-    public void OnClickEndListener() {
-        pickDate = (NumberPicker) findViewById(R.id.datePicker);
-        pickHour = (NumberPicker) findViewById(R.id.startHourPicker);
-        pickMin = (NumberPicker) findViewById(R.id.startMinutePicker);
-        Button btn = (Button) findViewById(R.id.setBookEnd);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Ulovlig å sette slutttid før starttid er satt (slik slepp vi å sjekke med current time)
-                TextView startText = (TextView) findViewById(R.id.starttimeString);
-                if (startText.getText().toString().length() > 0) {
-                    int sdValue = pickDate.getValue();
-                    int shValue = pickHour.getValue();
-                    int smValue = pickMin.getValue();
-                    if (sdValue > startDayValue) {
-                        writeEndTimeMessage();
-                    } else if (sdValue < startDayValue) {
-                        alertMsg('e');
-                    } else {
-                        if (shValue > startHourValue || shValue == startHourValue && smValue > startMinValue) {
-                            writeEndTimeMessage();
-                        } else {
-                            alertMsg('e');
-                        }
-                    }
-                } else {
-                    Toast.makeText(ProjectorBookForm.this, "Vennligst velg starttid først", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-    public void writeStartTimeMessage() {
+    public void writeStartTimeMessage(int dayValue, int hourValue, int minValue) {
         TextView startText = (TextView) findViewById(R.id.starttimeString);
-        int dayValue = pickDate.getValue();
         startDayValue = dayValue;
         String day = dateValues[dayValue];
-        int hourValue = pickHour.getValue();
         String trueHourValue = hourValues[hourValue];
         startHourValue = hourValue;
-        int minValue = pickMin.getValue();
         startMinValue = minValue;
         String trueMinValue = minValues[minValue];
+        this.startDayName = day;
         startText.setText(day + " " + trueHourValue + ":" + trueMinValue);
-    }
+        this.startMsgSet = true;
 
-    public void writeEndTimeMessage() {
-        final TextView endText = (TextView) findViewById(R.id.endtimeString);
-        int dayValue = pickDate.getValue();
-        endDayValue = dayValue;
-        String day = dateValues[dayValue];
-        int hourValue = pickHour.getValue();
-        String trueHourValue = hourValues[hourValue];
-        endHourValue = hourValue;
-        int minValue = pickMin.getValue();
-        endMinValue = minValue;
-        String trueMinValue = minValues[minValue];
-        endText.setText(day + " " + trueHourValue + ":" + trueMinValue);
+        //Endrar til duration
+        pickDate.setVisibility(View.INVISIBLE);
+        pickHour.setMaxValue(4);
+        pickHour.setDisplayedValues(hourDurations);
+        pickMin.setMaxValue(3);
+        pickMin.setDisplayedValues(minuteDurations);
+        Button btn = (Button) findViewById(R.id.setBookStart);
+        btn.setText("Sett varighet");
     }
-
-    private void alertMsg(char c) {
-        String msg = "";
-        if (c == 's') {
-            msg = "Verdien for starttid må være mindre enn slutttid";
-        } else if (c == 'e') {
-            msg = "Verdien for slutttid må være større enn starttid";
-        } else if (c == 'w') {
-            msg = "Verdien for starttid må være større enn klokkeslettet som er nå";
-        } else if (c == 'r') {
-            msg = "Vennligst velg starttid, slutttid og fyll inn beskrivelse";
+    public void writeEndTimeMessage(int shValue, int smValue){
+        TextView endText = (TextView) findViewById(R.id.endtimeString);
+        durationHour = shValue;
+        durationMin = smValue;
+        int endHourValue;
+        int endMinValue;
+        if(startHourValue + durationHour >= 24){
+            endHourValue = startHourValue + durationHour - 24;
+        }else{
+            endHourValue = startHourValue + durationHour;
         }
-        Toast.makeText(ProjectorBookForm.this, msg, Toast.LENGTH_SHORT).show();
+        if(startMinValue + durationMin >= 4){
+            endMinValue = startMinValue + durationMin - 4;
+            if(endHourValue + 1 >= 24){
+                endHourValue -= 23;
+            }else{
+                endHourValue += 1;
+            }
+        }else{
+            endMinValue = startMinValue + durationMin;
+        }
+        String endDay = "";
+        if(endHourValue < startHourValue){
+            int i = Arrays.asList(weekDays).indexOf(startDayName);
+            if(i + 1 > 6){
+                endDay = weekDays[0];
+            }else{
+                endDay = weekDays[i+1];
+            }
+        }else{
+            endDay = startDayName;
+        }
+        endText.setText(endDay + " " + hourValues[endHourValue] + ":" + minValues[endMinValue]);;
     }
 
     public void OnClickReserveListener() {
@@ -227,37 +198,41 @@ public class ProjectorBookForm extends AppCompatActivity {
                 int descriptionTextLength = bookDesEdit.getText().toString().length();
                 //Kontrollere om alle felt er utfylt før knappen kan trykkast
                 if (startTextLength > 0 && endTextLength > 0 && descriptionTextLength > 0) {
+                    //date
                     SimpleDateFormat bookDateFormat = new SimpleDateFormat("dd.MM.yyyy");
                     Calendar calStart = Calendar.getInstance(TimeZone.getTimeZone("Europe/Oslo"));
                     calStart.add(Calendar.DATE, startDayValue);
                     String bookStartDate = bookDateFormat.format(calStart.getTime());
+                    //startHour
                     String bookStartTime = "" + hourValues[startHourValue] + "." + minValues[startMinValue];
-
-                    Calendar calEnd = Calendar.getInstance(TimeZone.getTimeZone("Europe/Oslo"));
-                    calEnd.add(Calendar.DATE, endDayValue);
-                    String bookEndDate = bookDateFormat.format(calEnd.getTime());
-                    String bookEndTime = "" + hourValues[endHourValue] + ":" + minValues[endMinValue];
-
+                    //description
                     String bookText = bookDesEdit.getText().toString();
                     SharedPreferences prefs = getSharedPreferences("mypref", 0);
                     String roomNr = prefs.getString("roomnumber", "");
-
-                    writeToFireBase(bookStartDate, bookStartTime, bookEndDate, bookEndTime, roomNr, bookText);
+                    //duration
+                    String duration = "" + durationHour;
+                    if(durationMin == 1){
+                        duration += "25";
+                    }else if(durationMin == 2){
+                        duration += "5";
+                    }else if(durationMin == 3) {
+                        duration += "75";
+                    }
+                    writeToFireBase(bookStartDate, bookStartTime, duration, roomNr, bookText);
                     Intent intent = new Intent(ProjectorBookForm.this, ProjectorBookings.class);
                     startActivity(intent);
                 } else {
-                    alertMsg('r');
+                    Toast.makeText(ProjectorBookForm.this, "Vennligst velg starttid, slutttid og varighet", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    public void writeToFireBase(String bookStartDate, String bookStartTime, String bookEndDate, String bookEndTime, String roomNr, String comment) {
+    public void writeToFireBase(String bookStartDate, String bookStartTime, String duration, String roomNr, String comment) {
 
         // TODO: FIX THIS!!!!
         String userID = "SampleID";
-        String name = "Sølve's Tiny Pecker";
-        String duration = "1.5"; // TODO should be 1.5 aka 1.5hours aka 90min
+        String name = "Ola Nordmann";
 
         Firebase ref = new Firebase("https://teknobyen.firebaseio.com");
         Firebase newBooking = ref.child("reservations");
@@ -272,7 +247,6 @@ public class ProjectorBookForm extends AppCompatActivity {
         newBookingMap.put("duration", duration);
 
         newBooking.push().setValue(newBookingMap);
-
 
         /*** FORMAT
          userId: AAAA BAAA AAAG AA
