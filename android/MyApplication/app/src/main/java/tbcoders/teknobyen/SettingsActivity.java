8023ef5a8248ca194f1abc714dd147c0906d62fd
Alexolo <Alexolo1993@hotmail.com>
2016-05-30 21:05:01 +0200

    Altered settings to check if washingmachine credentials are valid

--- android/MyApplication/app/src/main/java/tbcoders/teknobyen/SettingsActivity.java
+++ android/MyApplication/app/src/main/java/tbcoders/teknobyen/SettingsActivity.java
@@ -1,12 +1,12 @@
 package tbcoders.teknobyen;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.Arrays;
@@ -19,8 +19,6 @@ public class SettingsActivity extends AppCompatActivity {
     private static EditText password;
     private static EditText roomNrField;
     private static EditText nameEdit;
-    String userS = "";
-    String userP = "";
     private static Button login_btn;
     private static Button cancel_btn;
     private static Button roomBTN;
@@ -29,15 +27,33 @@ public class SettingsActivity extends AppCompatActivity {
             401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422, 423, 424, 425, 426, 427,
             501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 512, 513, 514, 515, 516, 517, 518, 519, 520, 521, 522, 523,
             601, 602, 603, 604, 605, 606, 607, 608, 609, 610, 611, 612, 613, 614, 615, 616};
+    String userS = "";
+    String userP = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        fillTextFields();
         loginButton();
         roomButton();
 
     }
+
+    private void fillTextFields() {
+        roomNrField = (EditText) findViewById(R.id.roomNrEdit);
+        nameEdit = (EditText) findViewById(R.id.nameEdit);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        SharedPreferences prefs = getSharedPreferences("mypref", MODE_PRIVATE);
+        nameEdit.setText(prefs.getString("personname", ""));
+        roomNrField.setText(prefs.getString("roomnumber", ""));
+        username.setText(Base64EncryptDecrypt.decrypt(prefs.getString("username", "")));
+        password.setText(Base64EncryptDecrypt.decrypt(prefs.getString("password", "")));
+        checkHttpResponse(username.getText().toString(), password.getText().toString());
+    }
+
 
     public void loginButton() {
         username = (EditText) findViewById(R.id.username);
@@ -48,23 +64,27 @@ public class SettingsActivity extends AppCompatActivity {
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkHttpResponse(username.getText().toString(), password.getText().toString());
+
                 if (username.getText().toString().length() > 0 && password.getText().toString().length() > 0) {
-                    //For å lagre variabelar slik at dei skal vere tilgjengelige etter å ha lukka appen.
-                    SharedPreferences sharedPref = getSharedPreferences("mypref", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPref.edit();
-                    String userS = Base64EncryptDecrypt.encrypt(username.getText().toString());
-                    //krypterar passord med Base64
-                    String userP = Base64EncryptDecrypt.encrypt(password.getText().toString());
-                    editor.remove("username");
-                    editor.remove("password");
-                    editor.putString("username", userS);
-                    editor.putString("password", userP);
-                    editor.commit();
-                    Intent intent = new Intent(SettingsActivity.this, WashingMachine.class);
-                    startActivity(intent);
+                    if (checkHttpResponse(username.getText().toString(), password.getText().toString())) {
+                        //For å lagre variabelar slik at dei skal vere tilgjengelige etter å ha lukka appen.
+                        SharedPreferences sharedPref = getSharedPreferences("mypref", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        String userS = Base64EncryptDecrypt.encrypt(username.getText().toString());
+                        //krypterar passord med Base64
+                        String userP = Base64EncryptDecrypt.encrypt(password.getText().toString());
+                        editor.remove("username");
+                        editor.remove("password");
+                        editor.putString("username", userS);
+                        editor.putString("password", userP);
+                        editor.commit();
+                        // return to previous page
+                        Toast.makeText(SettingsActivity.this, "Gyldig bruker er registrert", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(SettingsActivity.this, "Brukernavn eller passord er feil, kan ikke autentisere!", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    Toast.makeText(SettingsActivity.this, "Feltene for brukernavn og passord kan ikke være tomme", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SettingsActivity.this, "Feltene for brukernavn og passord kan ikke være tomme.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -85,17 +105,17 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String personName = nameEdit.getText().toString();
                 String roomNr = roomNrField.getText().toString();
-                if (roomNr.length() == 3 && personName.length()>0) {
-                    if(Arrays.asList(roomNumbers).contains(Integer.valueOf(roomNr))){
+                if (roomNr.length() == 3 && personName.length() > 0) {
+                    if (Arrays.asList(roomNumbers).contains(Integer.valueOf(roomNr))) {
                         SharedPreferences sharedPref = getSharedPreferences("mypref", MODE_PRIVATE);
                         SharedPreferences.Editor editor = sharedPref.edit();
                         editor.remove("roomnumber");
                         editor.putString("roomnumber", roomNr);
                         editor.putString("personname", personName);
                         editor.commit();
-                        Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
-                        startActivity(intent);
-                    }else{
+                        // return to previous page
+                        Toast.makeText(SettingsActivity.this, "Navn og Romnummer er opdatert", Toast.LENGTH_SHORT).show();
+                    } else {
                         Toast.makeText(SettingsActivity.this, "Skriv inn gyldig romnummer", Toast.LENGTH_SHORT).show();
                     }
                 } else {
@@ -106,17 +126,21 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
 
-    private boolean checkHttpResponse(String username, String password){
+    private boolean checkHttpResponse(String username, String password) {
 
         AuthenticateUser authenticateUser = new AuthenticateUser();
+        ImageView check = (ImageView) findViewById(R.id.authenticationCheck);
         try {
             String statusCode = authenticateUser.execute(username, password).get();
-            if(statusCode.equals("200")){
+            if (statusCode.equals("200")) {
                 System.out.println("Code is good");
+                assert check != null;
+                check.setBackgroundColor(getResources().getColor(R.color.accepted));
                 return true;
-            }
-            else{
+            } else {
                 System.out.println("Code is bad " + statusCode);
+                assert check != null;
+                check.setBackgroundColor(getResources().getColor(R.color.declined));
                 return false;
             }
         } catch (InterruptedException | ExecutionException e) {
